9385b83b4e43e9bd1de4a7e8dedbb188498178fc
mmaecki <mateusz.miroslaw.malecki@gmail.com>
2023-11-21 12:59:53 +0100

    data analysis

--- {{cookiecutter.project_slug}}/dataset.py
+++ {{cookiecutter.project_slug}}/dataset.py
@@ -1,14 +1,29 @@
-from lightning import LightningDataModule
+import lightning as pl
+from datasets import load_dataset
 from torch.utils.data import DataLoader
 
+class CifarDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size: int = 32):
+        super().__init__()
+        self.batch_size = batch_size
+        self.dataset = load_dataset("cifar100").with_format("torch")
+        self.dataset = self.dataset.rename_columns({"img": "input", "fine_label": "target"})
+        self.dataset = self.dataset.remove_columns(["coarse_label"])
+        # self.setup()
 
-class MyDataModule(LightningDataModule):
-    def __init__(self):
-        super().__init__(...)
-        self.dataset = ...
+    def setup(self, stage: str):
+        self.train = self.dataset["train"]
+        self.test = self.dataset["test"]
 
     def train_dataloader(self):
-        return DataLoader(...)
+        return DataLoader(self.dataset["train"], batch_size=self.batch_size)
 
     def val_dataloader(self):
-        return DataLoader(...)
+        return DataLoader(self.dataset["test"], batch_size=self.batch_size)
+    
+    def log_params(self):
+        return {
+            "batch_size": self.batch_size,
+            "train_samples": len(self.dataset["train"]),
+            "val_samples": len(self.dataset["test"]),
+        }
--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -1,14 +1,36 @@
 from .models.base_model import Model
 from .dataset import MyDataModule
 
-from art.experiment.Experiment import ArtProject
+from dataset import CifarDataModule
+
+from art.project import ArtProject
+from art.checks import CheckResultExists, CheckScoreExists, CheckScoreLessThan, CheckScoreGreaterThan
+from art.steps import EvaluateBaseline, OverfitOneBatch, Overfit, TransferLearning
+from torchmetrics import Accuracy, Precision, Recall
+import torch.nn as nn
+from lightning.pytorch.callbacks import EarlyStopping
+
+
+from lightning import seed_everything
+seed_everything(23)
+
+from dataset import CifarDataModule
+
+from steps import DataAnalysis
 
 
 def main():
-    data_module = MyDataModule()
-    model = Model()
-    project = ArtProject("{{cookiecutter.project_slug}}", data_module)
-    project.add_step(...)
+    project = ArtProject("Cifar100", CifarDataModule(batch_size=32))
+
+    # Data Analysis
+    project.add_step(DataAnalysis(), [
+    CheckResultExists("number_of_classes"),
+    CheckResultExists("class_names"),
+    CheckResultExists("number_of_examples_in_each_class"),
+    CheckResultExists("img_dimensions")])
+
+    # Baseline
+    
     project.run_all()
 
 
