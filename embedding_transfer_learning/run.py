9b864be3acdd5068c36034dedebfc007d1046924
SebChw <sebastian.chwilczynski@gmail.com>
2023-12-07 15:23:19 +0100

    formatting

--- embedding_transfer_learning/run.py
+++ embedding_transfer_learning/run.py
@@ -1,61 +1,91 @@
-from art.checks import CheckResultExists, CheckScoreExists, CheckScoreCloseTo, CheckScoreLessThan, CheckScoreGreaterThan
-from art.steps import EvaluateBaseline, CheckLossOnInit, OverfitOneBatch, Overfit, Regularize
-from art.loggers import TensorBoardLoggerAdapter
+from dataset import EmbeddingDataModule
 from losses import ApproximateMRR
 from metrics import HitAtKMetric, MRRMetric
+from models.base_model import EmbeddingBaseline, EmbeddingHead
 from steps import DataPreparation, TextDataAnalysis
-from models.base_model import EmbeddingHead, EmbeddingBaseline
-from dataset import EmbeddingDataModule
 
+from art.checks import (
+    CheckResultExists,
+    CheckScoreCloseTo,
+    CheckScoreExists,
+    CheckScoreGreaterThan,
+    CheckScoreLessThan,
+)
+from art.loggers import TensorBoardLoggerAdapter
 from art.project import ArtProject
+from art.steps import (
+    CheckLossOnInit,
+    EvaluateBaseline,
+    Overfit,
+    OverfitOneBatch,
+    Regularize,
+)
 
 
 def getLogger(run_name: str):
     return TensorBoardLoggerAdapter(name=run_name)
 
+
 def main():
     project_name = "embedding_transfer_learning"
-    data_module = EmbeddingDataModule(batch_size=256)
+    data_module = EmbeddingDataModule(batch_size=256, num_workers=0)
     project = ArtProject(project_name, data_module)
-    project.add_step(DataPreparation(),[])
-    project.add_step(TextDataAnalysis(),[
-        CheckResultExists("embedding_size"),
-        CheckResultExists("number_of_reviews"),
-        CheckResultExists("average_review_length"),
-        CheckResultExists("average_query_length"),
-    ])
+    project.add_step(DataPreparation(), [])
+    # project.add_step(
+    #     TextDataAnalysis(),
+    #     [
+    #         CheckResultExists("embedding_size"),
+    #         CheckResultExists("number_of_reviews"),
+    #         CheckResultExists("average_review_length"),
+    #         CheckResultExists("average_query_length"),
+    #     ],
+    # )
     METRICS = [
         HitAtKMetric(top_k=1),
         HitAtKMetric(top_k=3),
         HitAtKMetric(top_k=10),
         MRRMetric(),
-        ApproximateMRR(100.0)
+        ApproximateMRR(100.0),
     ]
     project.register_metrics(METRICS)
     baseline = EmbeddingBaseline
     project.add_step(
         step=EvaluateBaseline(baseline),
-        checks=[CheckScoreExists(metric=METRICS[i])
-                for i in range(len(METRICS))],
+        checks=[CheckScoreExists(metric=METRICS[i]) for i in range(len(METRICS))],
     )
     model = EmbeddingHead
-    EXPECTED_LOSS = 1-0.70
+    EXPECTED_LOSS = 1 - 0.70
     project.add_step(
         CheckLossOnInit(model),
-        [CheckScoreCloseTo(metric=METRICS[4],
-                           value=EXPECTED_LOSS, abs_tol=0.1)]
+        [CheckScoreCloseTo(metric=METRICS[4], value=EXPECTED_LOSS, abs_tol=0.1)],
     )
     project.add_step(
-        step=OverfitOneBatch(model, model_kwargs=dict(lr=3e-4), number_of_steps=200, logger=getLogger("Overfit One Batch")),
-        checks=[CheckScoreLessThan(metric=METRICS[4], value=1-0.80)]
+        step=OverfitOneBatch(
+            model,
+            model_kwargs=dict(lr=3e-4),
+            number_of_steps=200,
+            logger=getLogger("Overfit One Batch"),
+        ),
+        checks=[CheckScoreLessThan(metric=METRICS[4], value=1 - 0.80)],
     )
     project.add_step(
-        step=Overfit(model, max_epochs=100, logger=getLogger("Overfit"), model_kwargs=dict(lr=3e-4), trainer_kwargs={"check_val_every_n_epoch": 1}),
-        checks=[CheckScoreLessThan(metric=METRICS[4], value=1-0.79)],
+        step=Overfit(
+            model,
+            max_epochs=100,
+            logger=getLogger("Overfit"),
+            model_kwargs=dict(lr=3e-4),
+            trainer_kwargs={"check_val_every_n_epoch": 1},
+        ),
+        checks=[CheckScoreLessThan(metric=METRICS[4], value=1 - 0.79)],
     )
     project.add_step(
-        Regularize(model, trainer_kwargs=dict(max_epochs=10), model_kwargs=dict(lr=1e-4), logger=getLogger("Regularize")),
-        checks=[CheckScoreGreaterThan(metric=METRICS[3], value=0.70)]
+        Regularize(
+            model,
+            trainer_kwargs=dict(max_epochs=10),
+            model_kwargs=dict(lr=1e-4),
+            logger=getLogger("Regularize"),
+        ),
+        checks=[CheckScoreGreaterThan(metric=METRICS[3], value=0.70)],
     )
     project.run_all()
 
--- embedding_transfer_learning/steps.py
+++ embedding_transfer_learning/steps.py
@@ -1,22 +1,22 @@
+import ast
 import random
 from pathlib import Path
 from typing import Dict
+
 import pandas as pd
 import torch
-from art.steps import Step, ExploreData
-import ast
-
+from common import DATAFRAME_PATH, QUERIES_PATH, RECIPIES_PATH
 from tqdm import tqdm
 from transformers import AutoModel
+from utils import batchify, extract_queries, make_recipe_string
 
-
-from common import DATAFRAME_PATH, RECIPE_EMBEDDINGS_PARQUET, FINAL_DATA_PARQUET_TRAIN, FINAL_DATA_PARQUET_VALID, FINAL_DATA_PARQUET_TEST
-from utils import batchify, make_recipe_string, extract_queries
 from art.loggers import art_logger
+from art.steps import ExploreData, Step
 
 
 class DataPreparation(Step):
     name = "Data Preparation"
+
     def __int__(self):
         super().__init__()
         self.dataset = None
@@ -57,14 +57,18 @@ class DataPreparation(Step):
 
     def calculate_recipe_embeddings(self, df, model, batch_size=2):
         all_data = []
-        for batch_df in tqdm(batchify(df, batch_size), total=(len(df)+batch_size-1)//batch_size):
-            embeddings = model.encode(batch_df['recipe_merged_info'].tolist())
-            #new df with id and embeddings
-            new_df = pd.DataFrame({
-                'id': batch_df['id'],
-                'recipe_embeddings': embeddings.tolist(),
-                'recipe_merged_info': batch_df['recipe_merged_info']
-            })
+        for batch_df in tqdm(
+            batchify(df, batch_size), total=(len(df) + batch_size - 1) // batch_size
+        ):
+            embeddings = model.encode(batch_df["recipe_merged_info"].tolist())
+            # new df with id and embeddings
+            new_df = pd.DataFrame(
+                {
+                    "id": batch_df["id"],
+                    "recipe_embeddings": embeddings.tolist(),
+                    "recipe_merged_info": batch_df["recipe_merged_info"],
+                }
+            )
             all_data.append(new_df)
 
         final_df = pd.concat(all_data, ignore_index=True)
@@ -72,8 +76,11 @@ class DataPreparation(Step):
 
     def calculate_query_embeddings(self, df, model, batch_size=512):
         final_df_parts = []
-        for batch_df in tqdm(batchify(df, batch_size), total=(len(df)+batch_size-1)//batch_size):
-            embeddings = model.encode(batch_df['query'].tolist())
+        for batch_df in tqdm(
+            batchify(df, batch_size), total=(len(df) + batch_size - 1) // batch_size
+        ):
+            queries = batch_df["query"].tolist()
+            embeddings = model.encode(queries)
             new_df = batch_df.copy()
             new_df["query_embeddings"] = embeddings.tolist()
             final_df_parts.append(new_df)
@@ -81,23 +88,9 @@ class DataPreparation(Step):
         final_df = pd.concat(final_df_parts, ignore_index=True)
         return final_df
 
-    def train_test_split(self, df):
-        recipe_ids = df["recipe_id"].unique()
-        random.shuffle(recipe_ids)
-        train_size = int(len(recipe_ids)*0.7)
-        valid_size = int(len(recipe_ids)*0.2)
-        test_sze = len(recipe_ids) - train_size - valid_size
-        train_ids = recipe_ids[:train_size]
-        valid_ids = recipe_ids[train_size:train_size+valid_size]
-        test_ids = recipe_ids[train_size+valid_size:]
-        train_df = df[df["recipe_id"].isin(train_ids)]
-        valid_df = df[df["recipe_id"].isin(valid_ids)]
-        test_df = df[df["recipe_id"].isin(test_ids)]
-        return train_df, valid_df, test_df
-
     def extract_queries(self, df):
         df["query"] = df["llm_output"].apply(extract_queries)
-        #queries is a column of lists make a separate row for each query
+        # queries is a column of lists make a separate row for each query
         df = df.explode("query")
         df = df[df["query"].str.len() > 0]
         df = df.reset_index(drop=True)
@@ -106,13 +99,15 @@ class DataPreparation(Step):
         return df
 
     def get_model(self):
-        model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-small-en', trust_remote_code=True)
+        model = AutoModel.from_pretrained(
+            "jinaai/jina-embeddings-v2-small-en", trust_remote_code=True
+        )
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model = model.to(device)
         return model
 
     def read_data(self):
-        LIST_COLUMNS = ['ingredients', 'steps', 'tags', 'nutrition']
+        LIST_COLUMNS = ["ingredients", "steps", "tags", "nutrition"]
         df = pd.read_csv(DATAFRAME_PATH)
         for list_column in LIST_COLUMNS:
             df[list_column] = df[list_column].apply(ast.literal_eval)
@@ -134,12 +129,16 @@ class TextDataAnalysis(ExploreData):
         self.datamodule.setup()
         train_dataloader = self.datamodule.train_dataloader()
         for batch in train_dataloader:
-            reviews.extend(batch['recipe_texts'])
-            queries.extend(batch['query_texts'])
-        self.embedding_size = train_dataloader.dataset[0]['recipe_embeddings'].shape[0]
+            reviews.extend(batch["recipe_texts"])
+            queries.extend(batch["query_texts"])
+        self.embedding_size = train_dataloader.dataset[0]["recipe_embeddings"].shape[0]
         self.number_of_reviews = len(reviews)
-        self.average_review_length = sum([len(review) for review in reviews]) / self.number_of_reviews
-        self.average_query_length = sum([len(query) for query in queries]) / self.number_of_reviews
+        self.average_review_length = (
+            sum([len(review) for review in reviews]) / self.number_of_reviews
+        )
+        self.average_query_length = (
+            sum([len(query) for query in queries]) / self.number_of_reviews
+        )
         self.results.update(
             {
                 "embedding_size": self.embedding_size,
@@ -151,4 +150,3 @@ class TextDataAnalysis(ExploreData):
 
     def log_params(self):
         return {}
-
